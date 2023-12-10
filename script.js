d103586ae42b0c0bcea05987140d6b2472497f04
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2023-12-10 22:03:09 +0330

    Update script.js
    update js file
    
    Signed-off-by: Tboompsh <145778331+Tboumpsh@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
+// Hello.
+// This project is a complete work with 'console log'.
+// This project is done in groups of 5 people.
+// Developers:
+// 1- Seer
+// 2-Nima
+// 3- Rahela
+// 4- AD
+// 5-Fatima
+// This project was made as an experiment and to work with structures.
+// email: navayearamm@gmail.com
+
+
+
+// create school object                                       
 //object 
 const school = {
-        //object
+        //object.this is object Because there are several other information in it that must be categorized. This object has a series of features
         classes: {
-            //array
+            // into our school have two classes
+            //array| Because have 4 students and many teachers
             classA: [
-                //index[0]
+                //index[0]|into a array this item in index[0]
                 info = {
                     totalStudent: 4,
-                    techerName: 'mr khateri',
+                    teacherName: 'mr khateri',
                     className: 'web devoloper'
                 },
-                //index[1]
+                //index[1]||into a array this item in index[1]
                 students = {
                     name: ['rahele', 'mobin', 'milad', 'nima'],
                     score: [20, 10, 12, 18],
@@ -18,15 +34,15 @@ const school = {
                 }
 
             ],
-            // araye 2
+             //array| Because have 4 students and many teachers
             classB: [
-                //index 1
+                //index[0]|into a array this item in index[0]
                 info = {
                     totalStudent: 4,
-                    techerName: 'mr khateri',
+                    teacherName: 'mr khateri',
                     className: 'javascript'
                 },
-                //index 2
+               //index[1]||into a array this item in index[1]
                 students = {
                     name: ['rahele', 'mobin', 'fatemeh', 'nima'],
                     score: [20, 10, 12, 18],
@@ -36,23 +52,44 @@ const school = {
             ]
         }
     }
-    // 1. let x='0021783738';
-    // console.log(typeof Number(x))
-
-console.log(school.classes.classA[0]);
-let x = school.classes.classA[1].id[0];
-console.log("nashnalId:", Number(x));
 
 
 
+// class A
+console.log( "classA informations => " , school.classes.classA[0]);
+console.log( "classA students property =>" ,school.classes.classA[1]);
+console.log( "classA | TeacherName is:" ,school.classes.classA[0].teacherName);
+console.log( "classA | Class name is:" ,school.classes.classA[0].className);
+console.log( "classA | the number of students is:" ,school.classes.classA[0].totalStudent);
+console.log( "classA students names >>" ,school.classes.classA[1].name);
+console.log( "classA students grads >>" ,school.classes.classA[1].score);
+
+
+// select array id and convert to number >>>>>>> line 64 to 69
+
+// one solve
+// let x = school.classes.classA[1].id[0];
+// console.log("national_ID:", Number(x));
+
+// two solve
+console.log( "ClassA | national_ID:" ,Number(school.classes.classA[1].id[1]));
+console.log( "ClassB | national_ID:" ,Number(school.classes.classB[1].id[0]));
+
+
+// classB
+console.log("classB informations => " , school.classes.classB[0]);
+console.log( "classB | TeacherName is:" ,school.classes.classB[0].teacherName);
+console.log( "classB | Class name is:" ,school.classes.classB[0].className);
+console.log("classB students property =>" , school.classes.classB[1]);
+console.log( "classB students names >>" ,school.classes.classB[1].name);
+console.log( "classB | The name of the group leader >>" ,school.classes.classB[1].name[3]);
+console.log( "classB mobin's grads >>" ,school.classes.classB[1].score[1]);
 
 
 
 
 
 
-//1. json.pars(json.string(id))
-// 2.use function 
 
 
 
@@ -65,6 +102,5 @@ console.log("nashnalId:", Number(x));
 
 
 
-// console.log(school.classes.classB[0]);
-// console.log(school.classes.classA[1]);
-// console.log(school.classes.classB[1]);
+
+
